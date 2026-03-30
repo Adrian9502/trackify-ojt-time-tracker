34ebc0ba771fb57d7e288e8ca6394246dfc3bebd
Adrian9502 <bontojohnadrian@gmail.com>
2026-03-30 23:19:58 +0800

    z

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -354,10 +354,10 @@ export default function CalendarPage() {
 
                     {hasEntries && (
                       <>
-                        <span className="text-base font-bold text-emerald-600 dark:text-emerald-400 leading-none">
+                        <span className="md:text-base text-xs font-bold text-emerald-600 dark:text-emerald-400 leading-none">
                           {formatHoursMinutes(dayData.totalHours)}
                         </span>
-                        <span className="hidden sm:block text-base  text-gray-400 dark:text-gray-500 group-hover:text-emerald-500 dark:group-hover:text-emerald-400 transition-colors mt-0.5 leading-none">
+                        <span className="hidden sm:block text-xs md:text-base  text-gray-400 dark:text-gray-500 group-hover:text-emerald-500 dark:group-hover:text-emerald-400 transition-colors mt-0.5 leading-none">
                           {dayData.count}{" "}
                           {dayData.count === 1 ? "entry" : "entries"}
                         </span>
